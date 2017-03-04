60a0993a98c9a582682cfde5f8fe4460688f3305
Вадим <guts.vadim@gmail.com>
2017-03-04 20:26:23 +0200

    module class

---
+++ Module.php
@@ -0,0 +1,13 @@
+<?php
+namespace bl\cms\shop\queen;
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
