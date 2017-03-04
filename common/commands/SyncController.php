476cf3115e73d4a7ec0a97629b1080c2aca710f4
Вадим <guts.vadim@gmail.com>
2017-03-05 00:59:05 +0200

    Currency sub-site synchronization implemented

--- common/commands/SyncController.php
+++ common/commands/SyncController.php
@@ -1,6 +1,11 @@
 <?php
 namespace bl\cms\shop\queen\common\commands;
+use bl\cms\shop\common\entities\Currency;
+use bl\cms\shop\queen\common\models\entities\ShopChildren;
+use bl\cms\shop\queen\common\models\entities\ShopChildrenSync;
+use bl\cms\shop\queen\common\models\entities\ShopQueenLog;
 use yii\console\Controller;
+use yii\httpclient\Client;
 
 /**
  * @author Gutsulyak Vadim <guts.vadim@gmail.com>
@@ -9,6 +14,44 @@ class SyncController extends Controller
 {
     public function actionIndex() {
         echo 'Sync...';
+
+        $sites = ShopChildren::find()->all();
+        foreach ($sites as $site) {
+            $logs = ShopQueenLog::findUnapplied($site);
+
+            echo count($logs) . ' logs to apply';
+
+            foreach ($logs as $log) {
+                $requestUrl = '';
+                $requestData = [];
+
+                switch ($log->entity_name) {
+                    case Currency::className(): {
+                        $requestUrl = '/subsite/rest/currency/update';
+                        $requestData = [
+                            'value' => Currency::findOne($log->entity_id)->value
+                        ];
+                        break;
+                    }
+                }
+
+                $client = new Client();
+                $response = $client->createRequest()
+                    ->setMethod('get')
+                    ->setUrl($site->domain_name . $requestUrl)
+                    ->setData($requestData)
+                    ->send();
+
+                echo $response->statusCode;
+
+                $sync = new ShopChildrenSync();
+                $sync->child_id = $site->id;
+                $sync->queen_log_id = $log->id;
+                $sync->status = $response->isOk ? ShopChildrenSync::STATUS_SUCCESS : ShopChildrenSync::STATUS_ERROR;
+                $sync->save();
+            }
+        }
+
         echo 'Done!';
     }
 }
