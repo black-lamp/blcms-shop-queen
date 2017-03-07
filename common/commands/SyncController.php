a9d17f9c756086d10be0ee9e86556a64ba337fd9
Вадим <guts.vadim@gmail.com>
2017-03-07 23:15:04 +0200

    added vendor sync

--- common/commands/SyncController.php
+++ common/commands/SyncController.php
@@ -1,11 +1,16 @@
 <?php
 namespace bl\cms\shop\queen\common\commands;
+
 use bl\cms\shop\common\entities\Currency;
+use bl\cms\shop\common\entities\ProductCountry;
+use bl\cms\shop\common\entities\Vendor;
 use bl\cms\shop\queen\common\models\entities\ShopChildren;
 use bl\cms\shop\queen\common\models\entities\ShopChildrenSync;
 use bl\cms\shop\queen\common\models\entities\ShopQueenLog;
 use yii\console\Controller;
+use yii\helpers\Console;
 use yii\httpclient\Client;
+use yii\httpclient\JsonParser;
 
 /**
  * @author Gutsulyak Vadim <guts.vadim@gmail.com>
@@ -13,13 +18,14 @@ use yii\httpclient\Client;
 class SyncController extends Controller
 {
     public function actionIndex() {
-        echo 'Sync...';
+        /* @var $sites ShopChildren[] */
 
+        $errors = 0;
         $sites = ShopChildren::find()->all();
         foreach ($sites as $site) {
             $logs = ShopQueenLog::findUnapplied($site);
 
-            echo count($logs) . ' logs to apply';
+            echo $site->domain_name . " - " . count($logs) . ' logs to apply:' . "\n";
 
             foreach ($logs as $log) {
                 $requestUrl = '';
@@ -33,25 +39,56 @@ class SyncController extends Controller
                         ];
                         break;
                     }
+                    case ProductCountry::className(): {
+                        $requestUrl = '/subsite/rest/country/update';
+                        $requestData = ProductCountry::find()
+                            ->where(['id' => $log->entity_id])
+                            ->with('translations')
+                            ->one();
+                        break;
+                    }
+                    case Vendor::className(): {
+                        $requestUrl = '/subsite/rest/vendor/update';
+                        $requestData = Vendor::find()
+                            ->where(['id' => $log->entity_id])
+                            ->with('translations')
+                            ->one();
+                        break;
+                    }
                 }
 
-                $client = new Client();
-                $response = $client->createRequest()
-                    ->setMethod('get')
-                    ->setUrl($site->domain_name . $requestUrl)
-                    ->setData($requestData)
-                    ->send();
+                $client = new Client([
+                    'requestConfig' => [
+                        'format' => Client::FORMAT_JSON
+                    ],
+                    'responseConfig' => [
+                        'format' => Client::FORMAT_JSON
+                    ],
+                ]);
 
-                echo $response->statusCode;
+                $message = $client->createRequest()
+                    ->setMethod('post')
+                    ->setFormat(Client::FORMAT_JSON)
+                    ->setUrl($site->domain_name . $requestUrl)
+                    ->setData($requestData);
+
+                $response = $message->send();
 
                 $sync = new ShopChildrenSync();
                 $sync->child_id = $site->id;
                 $sync->queen_log_id = $log->id;
                 $sync->status = $response->isOk ? ShopChildrenSync::STATUS_SUCCESS : ShopChildrenSync::STATUS_ERROR;
                 $sync->save();
+
+                if(!$response->isOk) {
+                    $errors++;
+                    $this->stdout("{$response->content} \n", Console::FG_GREY);
+                }
+
+                $this->stdout("{$response->statusCode} - {$site->domain_name}{$requestUrl} \n", Console::FG_GREY);
+//                $this->stdout("{$message->content} \n", Console::FG_GREY);
             }
         }
-
-        echo 'Done!';
+        $this->stdout("Done with {$errors} errors \n", Console::FG_GREY);
     }
 }
---
+++ common/listeners/Listener.php
@@ -0,0 +1,42 @@
+<?php
+namespace bl\cms\shop\queen\common\listeners;
+
+use bl\cms\shop\backend\components\events\CountryEvent;
+use bl\cms\shop\backend\components\events\VendorEvent;
+use bl\cms\shop\backend\controllers\CountryController;
+use bl\cms\shop\backend\controllers\CurrencyController;
+use bl\cms\shop\backend\controllers\VendorController;
+use bl\cms\shop\common\entities\Currency;
+use bl\cms\shop\queen\common\models\entities\ShopQueenLog;
+use yii\base\BootstrapInterface;
+use yii\base\Event;
+
+/**
+ * @author Gutsulyak Vadim <guts.vadim@gmail.com>
+ */
+
+class Listener implements BootstrapInterface
+{
+
+    public function bootstrap($app)
+    {
+        // Currency listener
+        Event::on(CurrencyController::className(),
+            CurrencyController::EVENT_AFTER_CHANGE, function() {
+                ShopQueenLog::log(Currency::findCurrent(), ShopQueenLog::ACTION_UPDATE);
+            });
+
+        // Country listener
+        Event::on(CountryController::className(),
+            CountryController::EVENT_AFTER_CREATE_OR_UPDATE_COUNTRY, function(CountryEvent $event) {
+                ShopQueenLog::log($event->country, ShopQueenLog::ACTION_UPDATE);
+            });
+
+        // Vendor listener
+        Event::on(VendorController::className(),
+            VendorController::EVENT_AFTER_CREATE_OR_UPDATE_VENDOR, function(VendorEvent $event) {
+                ShopQueenLog::log($event->vendor, ShopQueenLog::ACTION_UPDATE);
+            });
+    }
+
+}
